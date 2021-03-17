e19fca9e6b455caed1f1a5f1cdf9012e201fe5cf
Ruben Gutierrez <rubdeivis@gmail.com>
2021-03-17 19:40:44 -0400

    fix websocket

--- apps/room/routing.py
+++ apps/room/routing.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from room.consumers import KuropolyConsumer
+from apps.room.consumers import KuropolyConsumer
 
 websocket_urlpatterns = [
     url(r'^ws/play/(?P<idRoom>\w+)/$', KuropolyConsumer.as_asgi()),
--- kuropoly/asgi.py
+++ kuropoly/asgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-import room.routing
+import apps.room.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kuropoly.settings')
 
@@ -21,7 +21,7 @@ application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            room.routing.websocket_urlpatterns
+            apps.room.routing.websocket_urlpatterns
         )
     ),
 })
