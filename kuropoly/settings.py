86bddb80e4a297afc08f5fe072f23c5f396ab4cc
Dany Karam <danykaram97@gmail.com>
2021-03-20 01:15:48 -0400

    adding django-heroku

--- kuropoly/settings.py
+++ kuropoly/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -174,13 +173,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles"),
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+'''
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+'''
 MEDIA_URL = '/media/'
 
 AUTH_USER_MODEL = 'users.user'
 
 # Activate Django-Heroku.
+import django_heroku
 django_heroku.settings(locals())
