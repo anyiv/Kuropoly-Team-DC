327f59167c39cb2b2c90f38fd2ea352e597577ec
Dany Karam <danykaram97@gmail.com>
2021-03-23 21:10:27 -0400

    postgres

--- kuropoly/settings.py
+++ kuropoly/settings.py
@@ -112,14 +112,14 @@ CHANNEL_LAYERS = {
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -186,3 +186,6 @@ AUTH_USER_MODEL = 'users.user'
 # Activate Django-Heroku.
 import django_heroku
 django_heroku.settings(locals())
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
