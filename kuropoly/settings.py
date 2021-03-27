a534d06920cbeeb58890862423856321eb911b55
Dany Karam <danykaram97@gmail.com>
2021-03-27 18:18:59 -0400

    testing redis

--- kuropoly/settings.py
+++ kuropoly/settings.py
@@ -209,7 +209,6 @@ AUTH_USER_MODEL = 'users.user'
 import django_heroku
 django_heroku.settings(locals())
 
-'''
+
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-'''
