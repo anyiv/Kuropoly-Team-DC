4d46d802f5390ad5cc34f37d6dd484852343dafb
Dany Karam <danykaram97@gmail.com>
2021-03-26 22:25:34 -0400

    fixing transactions 2

--- kuropoly/settings.py
+++ kuropoly/settings.py
@@ -112,14 +112,14 @@ CHANNEL_LAYERS = {
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
+
 '''
 DATABASES = {
     'default': {
@@ -200,6 +200,7 @@ AUTH_USER_MODEL = 'users.user'
 import django_heroku
 django_heroku.settings(locals())
 
-
+'''
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+'''
