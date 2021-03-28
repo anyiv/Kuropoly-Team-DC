7204833250a2fea159c368cd4e97769118905043
Dany Karam <danykaram97@gmail.com>
2021-03-28 02:46:32 -0400

    merging

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: daphne kuropoly.asgi:application --port $PORT --bind 0.0.0.0 -v2
+web: gunicorn kuropoly.wsgi
--- kuropoly/settings.py
+++ kuropoly/settings.py
@@ -201,6 +201,5 @@ AUTH_USER_MODEL = 'users.user'
 import django_heroku
 django_heroku.settings(locals())
 
-
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
